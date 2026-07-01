b9dfcc9783eb083fbc8c118b46b74437414d95b6
Satish Mewada <mewadas494@gmail.com>
2026-07-01 15:50:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the Substrings/count-the-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int countSubstring(string S) {
+        int n =  S.size(),sum = 0,cnt = 0;
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        for(int i = 0;i<n;i++){
+            int var =- 1;
+            if(islower(S[i])) var=1;
+            sum+=var;
+            if(mp.find(sum)!=mp.end()){
+                cnt+=mp[sum];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+        
+        
+    }
+};
