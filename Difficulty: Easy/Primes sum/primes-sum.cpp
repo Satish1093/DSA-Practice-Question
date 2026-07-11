d4600ef270b2dd0584a43694000f302fb36fa90f
Satish Mewada <mewadas494@gmail.com>
2026-07-11 14:56:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Primes sum/primes-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    bool isSumOfTwo(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (isPrime(i) && isPrime(n - i))
+                return true;
+        }
+        return false;
+    }
+};
