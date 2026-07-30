fbad7523c11e82549e02dbf79a142e90302f6dc7
Satish Mewada <mewadas494@gmail.com>
2026-07-30 21:35:31 +0530

    Time: 0 ms (100%), Space: 9 MB (38.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(char ch :s){
-            if(ch == '('|| ch == '['|| ch == '{'){
-                st.push(ch);
-            }else{
-                if(st.empty()){
-                    return false;
-                }
-                char top = st.top();
-                st.pop();
-                if(ch == ')'&& top != '(')return false;
-                if(ch == ']'&& top != '[')return false;
-                if(ch == '}'&& top != '{')return false;
+      stack<char>st;
+      for(char ch :s){
+        if(ch == '('|| ch == '['|| ch == '{'){
+            st.push(ch);
+        }else{
+            if(st.empty()) return false;
+           if ((ch == ')' && st.top() != '(')||
+            (ch == ']' && st.top()!= '[')||
+            (ch == '}' && st.top() != '{')){
+                return false;
             }
+            st.pop();
         }
-        return st.empty();
+
+      }
+      return st.empty();
     }
 };
