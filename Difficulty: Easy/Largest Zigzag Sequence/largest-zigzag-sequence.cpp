9d757f7e8dee128814f45dc3fc86c3df6090d274
Satish Mewada <mewadas494@gmail.com>
2026-08-09 10:10:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest Zigzag Sequence/largest-zigzag-sequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int zigzagSequence(vector<vector<int>>& mat) {
+       int n = mat.size();
+       vector<vector<int>>dp(n,vector<int>(n,0));
+       for(int j= 0;j<n;j++){
+           dp[n-1][j] = mat[n-1][j];
+       }
+       for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+
+                int best = 0;
+
+                for (int k = 0; k < n; k++) {
+                    if (k != j) {
+                        best = max(best, dp[i + 1][k]);
+                    }
+                }
+
+                dp[i][j] = mat[i][j] + best;
+            }
+        }
+        int ans = 0;
+
+        for (int j = 0; j < n; j++) {
+            ans = max(ans, dp[0][j]);
+        }
+
+        return ans;
+    
+    }
+};
