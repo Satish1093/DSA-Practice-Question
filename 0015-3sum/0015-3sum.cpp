dafddeb92eb78b5701042148fc72533f716e8562
Satish Mewada <mewadas494@gmail.com>
2026-08-15 11:56:26 +0530

    Time: 50 ms (45.97%), Space: 28.9 MB (92.64%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+
         vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
+
         int n = nums.size();
 
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+        for(int i = 0; i < n - 2; i++) {
 
-            int j = i + 1;
-            int k = n - 1;
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            while(j < k) {
-                long long sum = (long long)nums[i] + nums[j] + nums[k];
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum == 0) {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++; 
-                    k--;
 
-    
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
+                    ans.push_back({
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    // Skip duplicate left values
+                    while(left < right &&
+                          nums[left] == nums[left + 1])
+                        left++;
+
+                    // Skip duplicate right values
+                    while(left < right &&
+                          nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
                 }
+
                 else if(sum < 0) {
-                    j++;
-                } 
+                    left++;
+                }
+
                 else {
-                    k--;
+                    right--;
                 }
             }
         }
+
         return ans;
     }
 };
