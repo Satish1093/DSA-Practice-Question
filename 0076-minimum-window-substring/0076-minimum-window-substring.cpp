2a3f92caddcebc50153042bdf65c9bf09d5e0281
Satish Mewada <mewadas494@gmail.com>
2026-08-16 11:40:24 +0530

    Time: 5 ms (64.85%), Space: 11.5 MB (43.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,60 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.length() < t.length()) {
+        if (t.size() > s.size()) return "";
+
+        vector<int> need(128, 0);
+        vector<int> window(128, 0);
+
+        for (char c : t) {
+            need[c]++;
+        }
+
+        int required = 0;
+
+        for (int i = 0; i < 128; i++) {
+            if (need[i] > 0)
+                required++;
+        }
+
+        int formed = 0;
+
+        int left = 0;
+        int start = 0;
+        int minLen = INT_MAX;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            char c = s[right];
+            window[c]++;
+
+            if (need[c] > 0 && window[c] == need[c]) {
+                formed++;
+            }
+
+            while (formed == required) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s[left];
+                window[leftChar]--;
+
+            
+                if (need[leftChar] > 0 &&
+                    window[leftChar] < need[leftChar]) {
+                    formed--;
+                }
+
+                left++;
+            }
+        }
+
+        if (minLen == INT_MAX)
             return "";
-        }
 
-        unordered_map<char, int> charCount;
-        for (char ch : t) {
-            charCount[ch]++;
-        }
-
-        int targetCharsRemaining = t.length();
-        int minWindow[2] = {0, INT_MAX};
-        int startIndex = 0;
-
-        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-            char ch = s[endIndex];
-            if (charCount.find(ch) != charCount.end() && charCount[ch] > 0) {
-                targetCharsRemaining--;
-            }
-            charCount[ch]--;
-
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s[startIndex];
-                    if (charCount.find(charAtStart) != charCount.end() && charCount[charAtStart] == 0) {
-                        break;
-                    }
-                    charCount[charAtStart]++;
-                    startIndex++;
-                }
-
-                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = startIndex;
-                    minWindow[1] = endIndex;
-                }
-
-                charCount[s[startIndex]]++;
-                targetCharsRemaining++;
-                startIndex++;
-            }
-        }
-
-        return minWindow[1] >= s.length() ? "" : s.substr(minWindow[0], minWindow[1] - minWindow[0] + 1);        
+        return s.substr(start, minLen);
     }
 };
