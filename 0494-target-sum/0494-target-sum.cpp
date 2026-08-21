da5d9cf8d1f4d76b1d570f9ce778241364f7e054
Satish Mewada <mewadas494@gmail.com>
2026-08-21 22:13:23 +0530

    Time: 1 ms (90.15%), Space: 12.5 MB (68.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& arr, int d) {
-          int n=arr.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+
+        for(int num : nums) {
+            sum += num;
         }
-        if(abs(d)>sum)return 0;
-        if((sum + d) % 2 != 0) return 0;
-        int s1=(sum+d)/2;
-        vector<vector<int>> dp(n + 1, vector<int>(s1 + 1, 0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
+
+        if(sum < abs(target)) return 0;
+
+        if((sum + target) % 2 != 0) return 0;
+
+        int tar = (sum + target) / 2;
+
+        vector<int> dp(tar + 1, 0);
+
+        dp[0] = 1;
+
+        for(int num : nums) {
+            for(int s = tar; s >= num; s--) {
+                dp[s] += dp[s - num];
+            }
         }
-        for(int i=1;i<=n;i++){
-             for(int j=0;j<=s1;j++){
-                 if(arr[i-1]>j){
-                     dp[i][j]=dp[i-1][j];
-                 }
-                 else{
-                     dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
-                 }
-             }
-        }
-        return dp[n][s1];
-    
+
+        return dp[tar];
     }
 };
