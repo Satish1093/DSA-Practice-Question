5dc2a85e91e4d6287a0520ff3302bb38430e9af8
Satish Mewada <mewadas494@gmail.com>
2026-08-23 16:58:35 +0530

    Time: 0 ms (100%), Space: 17.2 MB (41.18%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -4,25 +4,20 @@ public:
         int n = tokens.size();
         stack<int>st;
         for(string token : tokens){
-            if(token   == "+" || token == "-" || token == "*" || token == "/"){
+            if(token == "+" || token == "-" || token == "*" || token == "/"){
                 int b = st.top();
                 st.pop();
-
-                int a = st.top();
+                int a  = st.top();
                 st.pop();
                 if(token == "+")
                 st.push(a+b);
-
                 else if(token == "-")
                 st.push(a-b);
-
                 else if(token == "*")
                 st.push(a*b);
-
                 else
                 st.push(a/b);
-            }
-            else {
+            }else{
                 st.push(stoi(token));
             }
         }
