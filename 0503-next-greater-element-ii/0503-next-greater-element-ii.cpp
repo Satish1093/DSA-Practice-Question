e398777337b25396df98a12f188d392518348658
Satish Mewada <mewadas494@gmail.com>
2026-08-23 16:50:14 +0530

    Time: 119 ms (5.09%), Space: 27.8 MB (99.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,14 +4,13 @@ public:
         int n = nums.size();
         vector<int>ans(n,-1);
         for(int i =0;i<n;i++){
-            for(int j = 1;j<n;j++){
-            int index=(i+j)%n;
+            for(int j =1;j<n;j++){
+                int index = (i+j)%n;
                 if(nums[index] > nums[i]){
-                    ans[i]=nums[index];
+                    ans[i] = nums[index];
                     break;
-               
+                }
             }
-        }
         }
         return ans;
     }
