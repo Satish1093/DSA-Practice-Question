fff8e0b49e243a1e134fca333def31e794fb9292
Satish Mewada <mewadas494@gmail.com>
2026-08-24 00:09:18 +0530

    Time: 2 ms (80.58%), Space: 15.6 MB (51.8%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -4,14 +4,14 @@ void solve(int n ,int open,int close,string current,vector<string>&ans){
     if(current.size() == 2*n){
         ans.push_back(current);
         return;
-    }
-    if(open <n){
-        solve(n,open+1,close,current + '(' ,ans);
-    }
-    if(open > close){
-        solve(n,open ,close +1,current + ')',ans);
-    }
 
+    }
+    if(open<n){
+        solve(n,open+1,close,current + '(',ans);
+    }
+    if(open>close){
+        solve(n,open,close+1,current + ')',ans);
+    }
 }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
