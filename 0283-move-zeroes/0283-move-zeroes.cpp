85c1fa6dda5062fd67f7bd4d73cc15c39ff79bc0
Satish Mewada <mewadas494@gmail.com>
2026-08-31 13:29:31 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int  j =0;
         int n = nums.size();
-        int j = 0;;
-        for(int i=0;i<n;i++){
+        for(int i = 0;i<n;i++){
             if(nums[i] != 0){
                 swap(nums[i],nums[j]);
                 j++;
-        }
+            }
         }
     }
 };
