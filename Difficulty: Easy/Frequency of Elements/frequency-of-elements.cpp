71b2e4e25f6ba39caa2adaaf6a89b442788fc2e2
Satish Mewada <mewadas494@gmail.com>
2026-09-11 13:12:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequency of Elements/frequency-of-elements.cpp
+++ Difficulty: Easy/Frequency of Elements/frequency-of-elements.cpp
@@ -1,21 +1,18 @@
 class Solution {
   public:
     vector<vector<int>> countFreq(vector<int>& arr) {
-        
-         vector<vector<int>> res;
-        unordered_map<int, int> myMp;
-        
-        for(int i=0; i<arr.size(); i++){
-            myMp[arr[i]]++;
-        }
-        
-        for(int i=0; i<arr.size(); i++){
-            if(myMp[arr[i]] > 0){
-                res.push_back({arr[i], myMp[arr[i]]});
-                myMp[arr[i]]=0;
-            }
-        }
-        
-        return res;
+        map<int, int> mpp;
+
+              for (int i = 0; i < arr.size(); i++) {
+                  mpp[arr[i]]++;
+              }
+
+              vector<vector<int>> ans;
+
+              for (auto x : mpp) {
+                  ans.push_back({x.first, x.second});
+              }
+
+              return ans;
     }
 };
