227a29e7c970abcd3cad274ab8da6f0d6748e4f5
Satish Mewada <mewadas494@gmail.com>
2026-09-12 22:00:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -1,16 +1,17 @@
 class Solution {
   public:
     vector<int> leaders(vector<int>& arr) {
-       int n = arr.size();
-       int maxi = INT_MIN;
-       vector<int>ans;
-       for(int i = n-1;i>=0;i--){
-           if(arr[i]>=maxi){
-               ans.push_back(arr[i]);
-               maxi = arr[i];
-           }
-       }
-       reverse(ans.begin(),ans.end());
-       return ans;
+        int n =  arr.size();
+        vector<int>ans;
+        int maxi = INT_MIN;
+        for(int i =n-1;i>=0;i--){
+            if(arr[i] >= maxi){
+                ans.push_back(arr[i]);
+                  maxi =  arr[i];
+            }
+            
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
