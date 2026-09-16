8cce33fd939efcdf0c7763499625c4b3e36dfa86
Satish Mewada <mewadas494@gmail.com>
2026-09-16 11:58:31 +0530

    Time: 7 ms (85.08%), Space: 9.3 MB (77.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
+       int n = s.size();
         int start = 0,maxLen =1;
         for(int i = 0;i<n;i++){
             int l = i,r = i;
