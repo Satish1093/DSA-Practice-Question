4904a35da1e88f309eff04bb0f14cfcb51be5c42
Satish Mewada <mewadas494@gmail.com>
2026-09-16 09:18:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dominant Pairs/dominant-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dominantPairs(vector<int> &arr) {
+        int n = arr.size();
+        int mid = n / 2;
+
+        sort(arr.begin(), arr.begin() + mid);
+        sort(arr.begin() + mid, arr.end());
+
+        int count = 0;
+        int j = mid;
+
+        for (int i = 0; i < mid; i++) {
+
+            while (j < n && 5LL * arr[j] <= arr[i]) {
+                j++;
+            }
+
+            count += (j - mid);
+        }
+
+        return count;
+    }
+};
